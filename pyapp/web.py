4da3cce38309ea703cc64f40be8303d83bcd5e0f
Pepe Barbe <pepe@appfirst.com>
2015-06-04 23:24:17 -0500

    Refactor web app, and read enviromental parameters

--- pyapp/web.py
+++ pyapp/web.py
@@ -1,12 +1,27 @@
 import os
+import json
 
 from flask import Flask
 
-app = Flask(__name__)
+APP = Flask(__name__)
 
-@app.route("/")
+@APP.route("/")
 def hello():
     return "Hello World!"
 
+@APP.route("/services")
+def services():
+    return json.dumps(SERVICES, sort_keys=True, indent=4)
+
+def get_env_config(key, default_val=None, val_type=lambda x: x):
+    if key not in os.environ:
+        return default_val
+    return val_type(os.environ[key])
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=os.environ.get('PORT'))
+    HOST = get_env_config('VCAP_APP_HOST')
+    PORT = get_env_config('PORT', val_type=int)
+    SERVICES = get_env_config('VCAP_SERVICES', val_type=json.loads, default_val={})
+    DEBUG = get_env_config('APP_DEBUG', default_val=False, val_type=bool)
+
+    APP.run(host=HOST, port=PORT, debug=DEBUG)
